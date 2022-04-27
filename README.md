57192da3dc1bce69cb546e00257fd3ebae6139cb
B.N. Berrevoets <bert@berrevoets.net>
2022-04-27 15:21:26 +0200

    Video 3 - WYOOS 2 Done!

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Here is the link to the playlist:
 https://www.youtube.com/playlist?list=PLHh55M_Kq4OApWScZyPl5HhgsTJS9MZ6M
 
 * Video 1 Done!
+* Video 1B Done!
+* Video 2 Done!
 * 
--- kernel.cpp
+++ kernel.cpp
@@ -1,8 +1,10 @@
-void printf(char *str)
-{
-    unsigned short *VideoMemory = (unsigned short *)0xb8000;
+#include "types.h"
 
-    for (int i = 0; str[i] != '\0'; ++i)
+void print(int8_t *str)
+{
+    uint16_t *VideoMemory = (uint16_t *)0xb8000;
+
+    for (uint32_t i = 0; str[i] != '\0'; ++i)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
@@ -17,9 +19,9 @@ extern "C" void callConstructors()
     }
 }
 
-extern "C" void kernelMain(void *multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(const void *multiboot_structure, uint32_t magicnumber)
 {
-    printf("Hello World! --- http://www.berrevoets.net");
+    print("http://www.berrevoets.net");
 
     while (1)
         ;
