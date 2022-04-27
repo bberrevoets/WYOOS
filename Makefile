f462f6df89a0bf8f4f12f7ab2c3e3ae33a9a54b7
B.N. Berrevoets <bert@berrevoets.net>
2022-04-27 22:05:23 +0200

    Video 5 - WYOOS 4 Done!

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ GPPPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-excep
 ASPARAMS = --32
 LDPARAMS = -melf_i386
 
-objects = loader.o gdt.o kernel.o
+objects = loader.o gdt.o port.o kernel.o
 
 %.o: %.cpp
 	g++ $(GPPPARAMS) -o $@ -c $<
@@ -35,5 +35,6 @@ run: mykernel.iso
 	(killall virtualboxvm && sleep 1) || true
 	virtualboxvm --startvm "My Operating System" &
 
+.PHONY: clean
 clean:
-	rm *.o *.bin *.iso iso -rf
+	rm -f $(objects) mykernel.bin mykernel.iso
--- kernel.cpp
+++ kernel.cpp
@@ -1,12 +1,48 @@
 #include "types.h"
 #include "gdt.h"
 
-void print(int8_t *str)
+void printf(int8_t *str)
 {
     uint16_t *VideoMemory = (uint16_t *)0xb8000;
 
+    static uint8_t x = 0, y = 0;
+
     for (uint32_t i = 0; str[i] != '\0'; ++i)
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    {
+        switch (str[i])
+        {
+        case '\n':
+            x = 0;
+            y++;
+            break;
+
+        case '\t':
+            x += 4;
+            break;
+
+        default:
+            VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+            x++;
+            break;
+        }
+
+        if (x >= 80)
+        {
+            y++;
+            x = 0;
+        }
+
+        if (y >= 25)
+        {
+            for (y = 0; y < 25; y++)
+                for (x = 0; x < 80; x++)
+                {
+                    VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+                }
+            x = 0;
+            y = 0;
+        }
+    }
 }
 
 typedef void (*constructor)();
@@ -22,7 +58,8 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(const void *multiboot_structure, uint32_t magicnumber)
 {
-    print("http://www.berrevoets.net");
+    printf("Hello World! --- http://www.berrevoets.net\n");
+    printf("Hello World! --- http://www.berrevoets.net\n");
 
     GlobalDescriptorTable gdt;
 
