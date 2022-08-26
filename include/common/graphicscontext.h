bca3e55ddc07cbaf61c6b94346b392b25a8dde0c
B.N.Berrevoets <bert@berrevoets.net>
2022-08-26 21:34:30 +0200

    Video 12 - WYOOS 13 Done!

---
+++ include/common/graphicscontext.h
@@ -0,0 +1,14 @@
+#ifndef __WYOOS__COMMON__GRAPHICSCONTEXT_H
+#define __WYOOS__COMMON__GRAPHICSCONTEXT_H
+
+#include <drivers/vga.h>
+
+namespace wyoos
+{
+    namespace common
+    {
+        typedef wyoos::drivers::VideoGraphicsArray GraphicsContext;
+    }
+}
+
+#endif
